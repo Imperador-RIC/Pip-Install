85f378e5dc2fddc14496aaa672165fe674f9cd99
Imperador RIC <emailprofissionalric@gmail.com>
2023-12-05 23:57:25 -0300

    Bug fix
    Added a fix to the bug caused by the previous version, in which if the program fails on its first attempt to find the client's package manager, it terminates execution

--- source/main.py
+++ source/main.py
@@ -6,7 +6,7 @@ from re import match
 
 import subprocess
 
-package_managers = ['apk', 'apt', 'apt-get', 'dnf', 'emerge', 'pacman', 'pkg', 'yay', 'yum', 'zypper']
+package_managers = ['apk', 'apt', 'apt-get', 'dnf', 'emerge', 'pacman', 'pkg', 'yay', 'yum', 'zypper', None]
 
 if geteuid () == 0:
 
@@ -16,38 +16,44 @@ if geteuid () == 0:
     for package_manager in package_managers:
 
         try:
-            subprocess.check_output (f'which {package_manager}', shell = True)
 
-            if package_manager == 'apk':
-                syntax = 'add --no-cache'
-
-            elif package_manager == 'emerge':
-                syntax = '-a'
-
-            elif package_manager == 'yay' or package_manager == 'pacman':
-                syntax = '-S --noconfirm'
-
-            else:
-                syntax = 'install -y'
-
-            try:
-                with open (filename, 'r') as file:
-
-                    for package in file:
-
-                        package = match (r'^[a-zA-Z0-9-]+', package)
-                        subprocess.run (f'{package_manager} {syntax} python3-{package.group (0)}', shell = True)
-
-                    file.close ()
-                    exit ()
-
-            except FileNotFoundError:
-                print ('Error: File not found')
+            if package_manager == None:
+                print ('Error: No compatible package managers were found')
                 exit ()
 
+            else:
+
+                subprocess.check_output (f'which {package_manager}', shell = True)
+
+                if package_manager == 'apk':
+                    syntax = 'add --no-cache'
+
+                elif package_manager == 'emerge':
+                    syntax = '-a'
+
+                elif package_manager == 'yay' or package_manager == 'pacman':
+                    syntax = '-S --noconfirm'
+
+                else:
+                    syntax = 'install -y'
+
+                try:
+                    with open (filename, 'r') as file:
+
+                        for package in file:
+
+                            package = match (r'^[a-zA-Z0-9-]+', package)
+                            subprocess.run (f'{package_manager} {syntax} python3-{package.group (0)}', shell = True)
+
+                        file.close ()
+                        exit ()
+
+                except FileNotFoundError:
+                    print ('Error: File not found')
+                    exit ()
+
         except subprocess.SubprocessError:
-            print ('Error: No compatible package managers were found')
-            exit ()
+            pass
 
 else:
     print ('Error: Run the tool with administrator privileges')
